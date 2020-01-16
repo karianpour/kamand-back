21b595c4ad8ea7f004c4f39111e5bbdeb400f30e
Kayvan Arianpour <karianpour@gmail.com>
2020-01-16 18:03:54 +0330

    file upload bug fix

--- src/example/file-repository.ts
+++ src/example/file-repository.ts
@@ -20,7 +20,7 @@ class File implements Model {
   routes() {
     return [{
       method: 'POST' as HTTPMethod,
-      public: true,
+      public: false,
       url: '/:id',
       schema: {
       },
@@ -45,14 +45,18 @@ class File implements Model {
     console.log(`id: ${request.params.id}`);
 
     //K1 do something with the file
-    const actionParam = {};
+    const actionParam = {
+      file: request.raw.files.file,
+      id: request.params.id,
+      param1: request.body.param1,
+    };
 
     const result = await this.server.getDataService().act(this.address()+'/upload', actionParam, request.user);
     reply.send(result);
   }
 
   actUpload = async (client: PoolClient, actionParam: any, user: any) => {
-    const { id } = actionParam;
+    const { id, file, param1 } = actionParam;
 
     if(!(hasRole(user, 'admin'))){
       throw new Unauthorized(`only admin can execute this action!`);
@@ -62,17 +66,26 @@ class File implements Model {
       const f = 'id';
       throwError(f, 'required', `${f} is missing!`, `example.${f}`);
     }
+    if (!file) {
+      const f = 'file';
+      throwError(f, 'required', `${f} is missing!`, `example.${f}`);
+    }
+    if (!param1) {
+      const f = 'param1';
+      throwError(f, 'required', `${f} is missing!`, `example.${f}`);
+    }
 
     // await uniqueField(client, 'acc', 'id', 'code', id, code, 'example.code');
 
     //K1 do something with the file and save it to database, I am too lazy to create sample here
 
-    const result = await client.query({
-      text: ``,
-      values: [ id ],
-    });
+    // const result = await client.query({
+    //   text: ``,
+    //   values: [ id ],
+    // });
 
-    return result.rows.length > 0 ? result.rows[0] : null;
+    // return result.rows.length > 0 ? result.rows[0] : null;
+    return "ok";
   }
 }
 
@@ -83,6 +96,6 @@ export const models: Model[] = [
 
 /*
 
-curl -F fff=@./Pictures/20200114_162135.jpg -F param1=value1 -F param2=value2 -X POST http://localhost:8050/file/12344321
+curl -F file=@./Pictures/20200114_162135.jpg -F param1=value1 -F param2=value2 -X POST http://localhost:8050/file/12344321
 
 */
