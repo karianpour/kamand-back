1abb22659c859ab8eae785b845f696c4fb7fab82
Kayvan Arianpour <karianpour@gmail.com>
2020-06-09 08:30:45 +0430

    enhancing map to farsi

--- src/lib/utils/farsiUtils.ts
+++ src/lib/utils/farsiUtils.ts
@@ -1,9 +1,14 @@
 export function mapToFarsi(str: string | number | undefined | null): string | number | undefined | null {
   if (!str && str !== 0) return str;
-  return str.toString().replace(/[1234567890]/gi, e => String.fromCharCode(e.charCodeAt(0) + 1728))
+  return str.toString().replace(/[1234567890١٢٣٤٥٦٧٨٩٠]/gi, e => { const c = e.charCodeAt(0); return String.fromCharCode(c + (c < 60 ? 1728 : 144)) })
 }
 
 export function mapToLatin(str: string | number | undefined | null): string | number | undefined | null {
   if (!str && str !== 0) return str;
-  return str.toString().replace(/[۱۲۳۴۵۶۷۸۹۰]/gi, e => String.fromCharCode(e.charCodeAt(0) - 1728))
+  return str.toString().replace(/[۱۲۳۴۵۶۷۸۹۰١٢٣٤٥٦٧٨٩٠]/gi, e => { const c = e.charCodeAt(0); return String.fromCharCode(c - (c < 1770 ? 1584 : 1728)) })
+}
+
+export function stripAnyThingButDigits(str: string): string {
+  if(!str) return str;
+  return str.toString().replace(/[^1234567890۱۲۳۴۵۶۷۸۹۰١٢٣٤٥٦٧٨٩٠]/gi, '');
 }
