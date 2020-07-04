be3bc1e02672f082cbcaaeb7285ed4d4f5443d75
Kayvan Arianpour <karianpour@gmail.com>
2020-07-05 01:26:24 +0430

    better snake case field util

--- src/lib/utils/dbUtils.ts
+++ src/lib/utils/dbUtils.ts
@@ -1,3 +1,9 @@
 import { snakeCase } from 'change-case';
 
-export const snakeCasedFields = (fields:string[]) => fields.map(f => {const sf = snakeCase(f); return sf===f ? f : `${sf} as "${f}"`});
+export const snakeCasedFields = (fields:string[]) => fields.map(snakeCasedField);
+
+export const snakeCasedField = (f: string): string => {
+  if(f.indexOf(' as ') > -1) return f;
+  const sf = snakeCase(f);
+  return sf===f ? f : `${sf} as "${f}"`;
+}
