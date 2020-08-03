4a4c3f195b7be6a6931409d57db4963cc63fc99f
Kayvan Arianpour <karianpour@gmail.com>
2020-08-03 12:07:15 +0430

    login bug fix, loger false sample

--- src/example/index.ts
+++ src/example/index.ts
@@ -5,7 +5,11 @@ import {App} from '../lib/app';
 
 async function main (){
   const app = new App();
-  await app.init(__dirname);
+  await app.init(__dirname, {
+    fastify: {
+      logger: false,
+    }
+  });
   app.listenNetwork();
 }
 
